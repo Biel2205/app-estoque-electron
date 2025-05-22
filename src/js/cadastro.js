c12ac7f29e9723138ff590ef8fb6841188afcbfb
gabriel <fulanodetal@exemplo.br>
2025-05-21 22:05:31 -0300

    hoje dois

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -1,2 +1,24 @@
 var formProduto = document.getElementById("form-produto")
-formProduto.addEventListener
+
+function cadastrarProduto(event) {
+
+    event.preventDefault ()
+
+    var produto = document.getElementById('nome-produto').value
+
+    var quantidade = document.getElementById("quantidade-produto").value
+
+
+
+
+
+    console.log(produto)
+    console.log(quantidade)
+
+    console.log("Função cadastrarProduto chamada")
+
+
+
+}
+
+formProduto.addEventListener("submit", cadastrarProduto)
